6807be90d263635c20fb7df7ebc35e2c3e33862a
Eduardo Santana <edusant999@gmail.com>
2026-06-25 21:25:41 +0000

    chore: merge alembic heads

---
+++ alembic/versions/765cbcbcb6b1_merge_heads.py
@@ -0,0 +1,28 @@
+"""merge heads
+
+Revision ID: 765cbcbcb6b1
+Revises: b3f8a2c91d47, f5935b2973e6
+Create Date: 2026-06-25 21:15:36.969972
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '765cbcbcb6b1'
+down_revision: Union[str, Sequence[str], None] = ('b3f8a2c91d47', 'f5935b2973e6')
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
